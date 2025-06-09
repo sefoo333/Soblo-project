c6c385734d0d65a324c22337b1c4717090888672
Seifeldeen Ali <111438242+sefoo333@users.noreply.github.com>
2025-06-09 10:15:22 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 import Navbar from "./_componants/small_comps/Navbar"
 import Home_comp from "./_componants/pages/Home_comp";
 
- export default async function Home() {
+ export default function Home() {
 
   return (
    <>
-           <Navbar />
+{/*            <Navbar /> */}
    <div className="parent max-md:px-[20px] w-full flex justify-center">
 <div className="container  w-[80rem] py-10">
   <Home_comp />

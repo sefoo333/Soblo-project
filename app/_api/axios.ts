4eab2f4d7a108b771a2fd96bacb4f8509c3d1d1e
Seifeldeen Ali <111438242+sefoo333@users.noreply.github.com>
2025-06-09 23:30:56 +0300

    Update axios.ts

--- app/_api/axios.ts
+++ app/_api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://soblo-backend-production.up.railway.app/api",
+  baseURL: process.env.api_soblo,
   headers: {
     'Content-Type': 'application/json',
   },
